4ff331e6aac4660f38a0b36756b94778c6fbeb10
aksel26 <kevinxkim2023@gmail.com>
2019-11-24 01:36:00 +0900

    test

---
+++ (1108~) Python/(1115 ~) CRUD/movie/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hk\docume~1\github\til\(1108~~1\(1115~~1\movie\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

3a6abe50d365b4780e8e39dc448ba179d8e9ceb7
aksel26 <kevinxkim2023@gmail.com>
2019-11-28 09:33:39 +0900

    initiate

---
+++ (1108~) Python/(1128~) 관계/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hk\docume~1\github\til\(1108~~1\(1128~~1\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

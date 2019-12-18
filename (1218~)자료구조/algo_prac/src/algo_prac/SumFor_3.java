a63e1d4aad29a4d54b02a5f1f5ffe031db78849f
aksel26 <kevinxkim2023@gmail.com>
2019-12-18 18:58:18 +0900

    for연습문제

---
+++ (1218~)자료구조/algo_prac/src/algo_prac/SumFor_3.java
@@ -0,0 +1,40 @@
+package algo_prac;
+
+import java.util.Scanner;
+
+
+//매개변수 a,b 합
+//정수 a,b 포함 그 사이 정수의 합을 구해 반환하는 메서드 작성
+
+public class SumFor_3 {
+	static int sumof(int a, int b) {
+		int min =0;
+		int max =0;
+		if(a>b) {
+			min=b;
+			max=a;
+			
+		}
+		else {
+			min =a;
+			max =b;
+			
+		}
+		int sum =0;
+		for (int i =min;i<=max;i++) {
+			sum+=i;
+		}
+		return(sum);
+		
+	}
+	
+	
+	public static void main(String[] args) {
+
+		System.out.println("sumof(2,3) = " +sumof(3,2));
+		
+	}
+	
+	
+
+}

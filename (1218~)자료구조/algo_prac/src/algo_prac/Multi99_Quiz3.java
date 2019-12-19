5ca7f64bc3d14a468188e4e0528c7170a87194d7
aksel26 <kevinxkim2023@gmail.com>
2019-12-19 17:41:45 +0900

    triangle

---
+++ (1218~)자료구조/algo_prac/src/algo_prac/Multi99_Quiz3.java
@@ -0,0 +1,26 @@
+package algo_prac;
+
+public class Multi99_Quiz3 {
+
+	public static void main(String[] args) {
+		//다중 루프 : 반복이 중첩되는 것
+		
+		for(int i=1;i<=5;i++) {
+			
+			for(int j=1; j<5; j++) {
+				System.out.print("*");
+			}
+			System.out.println("*");
+		}
+		
+
+	}
+
+}
+
+//   출력
+//	*****
+//	*****
+//	*****
+//	*****
+//	*****
---
+++ (1218~)자료구조/algo_prac/src/algo_prac/TriangleLB.java
@@ -0,0 +1,26 @@
+package algo_prac;
+
+import java.util.Scanner;
+
+public class TriangleLB {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n ;
+		do {
+			System.out.println("몇 단 삼각형입니까?");
+			n = scn.nextInt();
+			
+		}while(n<=0);
+		
+		for(int i=1; i<=n; i++) {
+			
+			for (int j =1 ; j<=i; j++) {
+				System.out.print("*");}
+			System.out.println();
+		}
+
+
+	}
+
+}

99ddbeb781c4124d778c9f01849178d685219fb4
aksel26 <kevinxkim2023@gmail.com>
2019-12-20 04:27:52 +0900

    Update Array_Max_Min_2.java

--- (1218~)자료구조/algo_prac/src/arrays/Array_Max_Min_2.java
+++ (1218~)자료구조/algo_prac/src/arrays/Array_Max_Min_2.java
@@ -20,9 +20,8 @@ public class Array_Max_Min_2 {
 		Scanner scn = new Scanner(System.in);
 		
 		System.out.println("키의 최댓값을 구합니다");
-		System.out.print("사람 수 : ");
-		
-		int num= scn.nextInt();
+		int num= rand.nextInt(10);
+		System.out.print("사람 수 : " +num + "명");
 		
 		int height[]=new int[num];
 		
@@ -39,6 +38,8 @@ public class Array_Max_Min_2 {
 }
 
 
+//인원 난수 설정 전
+
 //키의 최댓값을 구합니다
 //사람 수 : 5
 //키 값은 아래와 같습니다
@@ -48,3 +49,17 @@ public class Array_Max_Min_2 {
 //height[3] : 138
 //height[4] : 143
 //최댓값은 171 입니다
+
+
+//인원 난수 설정 후
+//
+//키의 최댓값을 구합니다
+//사람 수 : 7명키 값은 아래와 같습니다
+//height[0] : 122
+//height[1] : 105
+//height[2] : 127
+//height[3] : 149
+//height[4] : 166
+//height[5] : 116
+//height[6] : 149
+//최댓값은 166 입니다

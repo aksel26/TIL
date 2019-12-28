cc0bde478ff274f4aaf307d746cd9d4d8050d09e
aksel26 <kevinxkim2023@gmail.com>
2019-12-29 00:47:33 +0900

    단순 선택 정렬

--- (1218~)DataStructure/src/sort/BubbleSort.java
+++ (1218~)DataStructure/src/sort/BubbleSort.java
@@ -35,6 +35,38 @@ public class BubbleSort {
 			if(exchg==0)break;
 		}
 	}
+	
+	
+	static void bubbleSortV3(int[]a, int n) {
+		int k = 0; //a[k] 보다 앞쪽은 정렬을 마친 상태, 0으로 하는 이유는 첫 번째 패스에서는 모든 요소를 검사해야 하기 때문
+		
+		
+		while(k<n-1) {
+			int last = n-1; // 마지막으로 요소를 교환한 위치, 각 패스에서 마지막으로 교환한 두 요소 가운데 오른쪽요소 (a[j])의 인덱스 저장.
+			for(int j = n-1; j>k; j--) {
+				if(a[j-1]>a[j]) {
+					swap(a,j-1,j);
+					last=j;
+				}
+			}
+			k = last; //범위 제한
+		}
+	}
+	
+	
+	
+	//단순 선택 정
+	static void selectionSort(int[]a , int n) {
+		for (int i = 0;i<n-1; i++) {
+			int min = i;
+			for(int j= i+1; j<n ; j++) {
+				if (a[j]<a[min]) {
+					min = j;
+				}
+			}
+			swap(a,i,min);
+		}
+	}
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
@@ -49,7 +81,7 @@ public class BubbleSort {
 			x[i] = scn.nextInt();
 		}
 
-		bubbleSortV2(x, n);
+		selectionSort(x, n);
 
 		System.out.println(" 오름차순으로 정렬 완료");
 		for (int i = 0; i < n; i++) {

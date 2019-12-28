7700f171dfad49c844ae05f6776637c6feb178e6
aksel26 <kevinxkim2023@gmail.com>
2019-12-28 17:36:31 +0900

    Hanoi

--- (1218~)DataStructure/src/recursive/Recur.java
+++ (1218~)DataStructure/src/recursive/Recur.java
@@ -2,6 +2,8 @@ package recursive;
 
 import java.util.Scanner;
 
+import Stack_Queue.IntStack;
+
 public class Recur {
 	static void recur(int n) {
 		if (n > 0) {
@@ -10,16 +12,33 @@ public class Recur {
 			recur(n - 2);
 		}
 	}
-	
-	
-	//꼬리재귀를 제거함.
+
+	// 꼬리재귀를 제거함.
 	static void recur2(int n) {
-		if (n>0) {
-			recur(n-1);
+		if (n > 0) {
+			recur(n - 1);
 			System.out.println("n");
 
-			//여가
-			n =n-2;
+			// 여가
+			n = n - 2;
+		}
+	}
+
+	static void recur3(int n) {
+		IntStack s = new IntStack(n);
+		while (true) {
+			if (n > 0) {
+				s.push(n);
+				n = n - 1;
+				continue;
+			}
+			if (s.isEmpty() != true) {
+				n = s.pop();
+				System.out.println(n);
+				n = n - 2;
+				continue;
+			}
+			break;
 		}
 	}
 
@@ -28,11 +47,10 @@ public class Recur {
 		System.out.println("정수를 입력하세요");
 		int num = scn.nextInt();
 
-		recur(num);
+		recur3(num);
 	}
 
 }
-
 
 //	정수를 입력하세요
 //	4

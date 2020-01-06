8e881da619e52f547bb3271dd07e66f2b3a67a85
aksel26 <kevinxkim2023@gmail.com>
2020-01-06 17:04:48 +0900

    Update Stack_prac.java

--- (20_0103)Algorithm_Baejoon/src/Basic/Stack_prac.java
+++ (20_0103)Algorithm_Baejoon/src/Basic/Stack_prac.java
@@ -10,6 +10,31 @@ import java.util.Stack;
 
 public class Stack_prac {
 
+	
+	
+	//괄호 
+	
+	public static String isValid(String s) {
+		int n = s.length();
+		int cnt = 0;
+		for(int i = 0; i< n; i++) {
+			if(s.charAt(i)=='(') {
+				cnt +=1;
+			}else {
+				cnt -=1;
+			}
+			if(cnt<0) {
+				return "NO";
+			}
+			
+		}
+		if(cnt==0) {
+			return "YES";
+		}else {
+			return "NO";
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 
 //		스택의 구현
@@ -39,42 +64,42 @@ public class Stack_prac {
 //		출력
 //		출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력한다.
 //		
-		Scanner scn = new Scanner(System.in);
-
-		int n = scn.nextInt();
-
-		int[] stack = new int[n];
-		int size = 0;
-		while (n-- > 0) {
-			String cmd = scn.next();
-			if (cmd.equals("push")) {
-				int num = Integer.parseInt(scn.next());
-				stack[size++] = num;
-			} else if (cmd.equals("top")) {
-				if (size == 0) {
-					System.out.println("-1");
-				} else {
-					System.out.println(stack[size - 1]);
-				}
-			} else if (cmd.equals("size")) {
-				System.out.println(size);
-
-			} else if (cmd.equals("empty")) {
-				if (size == 0) {
-					System.out.println("1");
-				} else {
-					System.out.println("0");
-				}
-			} else if (cmd.equals("pop")) {
-				if (size == 0) {
-					System.out.println("-1");
-				} else {
-					System.out.println(stack[size - 1]);
-					size -= 1;
-				}
-			}
-
-		}
+//		Scanner scn = new Scanner(System.in);
+//
+//		int n = scn.nextInt();
+//
+//		int[] stack = new int[n];
+//		int size = 0;
+//		while (n-- > 0) {
+//			String cmd = scn.next();
+//			if (cmd.equals("push")) {
+//				int num = Integer.parseInt(scn.next());
+//				stack[size++] = num;
+//			} else if (cmd.equals("top")) {
+//				if (size == 0) {
+//					System.out.println("-1");
+//				} else {
+//					System.out.println(stack[size - 1]);
+//				}
+//			} else if (cmd.equals("size")) {
+//				System.out.println(size);
+//
+//			} else if (cmd.equals("empty")) {
+//				if (size == 0) {
+//					System.out.println("1");
+//				} else {
+//					System.out.println("0");
+//				}
+//			} else if (cmd.equals("pop")) {
+//				if (size == 0) {
+//					System.out.println("-1");
+//				} else {
+//					System.out.println(stack[size - 1]);
+//					size -= 1;
+//				}
+//			}
+//
+//		}
 
 
 
@@ -117,27 +142,54 @@ public class Stack_prac {
 //		출력
 //		각 테스트 케이스에 대해서, 입력으로 주어진 문장의 단어를 모두 뒤집어 출력한다.
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int i = Integer.parseInt(br.readLine());
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		while (i-- > 0) {
-			String str = br.readLine() + "\n";
-			Stack<Character> s = new Stack<>();
-			for (char ch : str.toCharArray()) {
-				if (ch == '\n' || ch == ' ') {
-					while (!s.isEmpty()) {
-						bw.write(s.pop());
-					}
-					bw.write(ch);
-				} else {
-					s.push(ch);
-				}
-			}
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int i = Integer.parseInt(br.readLine());
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		while (i-- > 0) {
+//			String str = br.readLine() + "\n";
+//			Stack<Character> s = new Stack<>();
+//			for (char ch : str.toCharArray()) {
+//				if (ch == '\n' || ch == ' ') {
+//					while (!s.isEmpty()) {
+//						bw.write(s.pop());
+//					}
+//					bw.write(ch);
+//				} else {
+//					s.push(ch);
+//				}
+//			}
+//
+//		}
+//
+//		bw.flush();
 
+		
+		
+//		9012번
+//		괄호 문자열(Parenthesis String, PS)은 두 개의 괄호 기호인 ‘(’ 와 ‘)’ 만으로 구성되어 있는 문자열이다. 
+//		그 중에서 괄호의 모양이 바르게 구성된 문자열을 올바른 괄호 문자열(Valid PS, VPS)이라고 부른다. 
+//		한 쌍의 괄호 기호로 된 “( )” 문자열은 기본 VPS 이라고 부른다. 
+//		만일 x 가 VPS 라면 이것을 하나의 괄호에 넣은 새로운 문자열 “(x)”도 VPS 가 된다. 그리고 두 VPS x 와 y를 접합(concatenation)시킨 새로운 문자열 xy도 VPS 가 된다. 
+//		예를 들어 “(())()”와 “((()))” 는 VPS 이지만 “(()(”, “(())()))” , 그리고 “(()” 는 모두 VPS 가 아닌 문자열이다. 
+//
+//		여러분은 입력으로 주어진 괄호 문자열이 VPS 인지 아닌지를 판단해서 그 결과를 YES 와 NO 로 나타내어야 한다. 
+//		
+//		입력
+//		입력 데이터는 표준 입력을 사용한다. 입력은 T개의 테스트 데이터로 주어진다. 입력의 첫 번째 줄에는 입력 데이터의 수를 나타내는 정수 T가 주어진다. 
+//		각 테스트 데이터의 첫째 줄에는 괄호 문자열이 한 줄에 주어진다. 하나의 괄호 문자열의 길이는 2 이상 50 이하이다. 
+//		
+//		출력
+//		출력은 표준 출력을 사용한다. 만일 입력 괄호 문자열이 올바른 괄호 문자열(VPS)이면 “YES”, 아니면 “NO”를 한 줄에 하나씩 차례대로 출력해야 한다. 
+		
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		while(n-->0) {
+			System.out.println(isValid(scn.next()));
 		}
-
-		bw.flush();
-
+		
+		
+		
+		
 	}
 
 }

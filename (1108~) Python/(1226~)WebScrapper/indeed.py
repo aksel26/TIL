6ae2e15b97674efe035a81f01b4205152cf54fb0
aksel26 <kevinxkim2023@gmail.com>
2020-01-08 18:58:38 +0900

    extract company

--- (1108~) Python/(1226~)WebScrapper/indeed.py
+++ (1108~) Python/(1226~)WebScrapper/indeed.py
@@ -30,7 +30,7 @@ def extract_indeed_pages():
 
 
 
-#### Extracting title ######
+#### Extracting title  & Extacting Companies ##########
 # BeautifulSoup 를 이용!
 
 
@@ -38,13 +38,31 @@ def extract_indeed_jobs(last_page):
     jobs=[]
     for page in range(last_page):
 
-        result=requests.get(f"{URL}&start={page*LIMIT}")
+        result=requests.get(f"{URL}&start={0*LIMIT}")
         soup=BeautifulSoup(result.text,"html.parser")
         results = soup.find_all("div",{"class":"jobsearch-SerpJobCard"})
         for result in results:
             title= result.find("div",{"class":"title"}).find("a")["title"]
-        
-            print(title)
+            company = result.find("span",{"class":"company"})
+            company_anchor=company.find("a")
+            if company_anchor is not None :
+                company = str(company_anchor.string)
+            else:
+                company= str(company.string)
+            company= company.strip()
+            print(title,company)
         return jobs
         
 
+
+# 출력
+# Jr Python Developer Ace-stack LLC
+# R&D Imagineer Walt Disney Imagineering
+# Python Developer Intern OXO Solutions
+# Junior Python Coder Social Commerce Ventures
+# Reservoir Engineer - Algorithm Development Beyond Limits
+# MongoDB, Python Developer Evolent Health
+# Legal Investigator and Threat Researcher Facebook
+# Data Scientist (Python) Applied Memetics LLC
+# Software Engineer -Python/MongoDB/NodeJS Evolent Health
+# Students - Equity Trading Desk Internship Summer 2019 SMB Capital

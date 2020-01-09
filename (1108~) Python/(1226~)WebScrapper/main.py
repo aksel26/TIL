a463d840abed1a67f9b0dce6de776f842801ace5
aksel26 <kevinxkim2023@gmail.com>
2020-01-09 19:17:54 +0900

    finish scrapping

--- (1108~) Python/(1226~)WebScrapper/main.py
+++ (1108~) Python/(1226~)WebScrapper/main.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
 from indeed import extract_indeed_pages,extract_indeed_jobs
 
 # max_indeed_pages=extract_indeed_pages()
 
 last_indeed_page = extract_indeed_pages()
 
-extract_indeed_jobs(last_indeed_page)
+
+indeed_jobs= extract_indeed_jobs(last_indeed_page)
+print(indeed_jobs)

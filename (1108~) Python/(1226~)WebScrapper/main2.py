882285fc7d621a2dd9e39b7411a9c0c16ceb188b
aksel26 <kevinxkim2023@gmail.com>
2020-01-12 21:31:57 +0900

    object method

--- (1108~) Python/(1226~)WebScrapper/main2.py
+++ (1108~) Python/(1226~)WebScrapper/main2.py
@@ -1,6 +1,38 @@
-def plus(*args):
-    result = 0 
-    for number in args:
-        result += number
-        print(result)
-plus(1,2,1,1,1,1,1,1,1,1,11,4,3,5,6,3)
+class Car():
+    wheels =4
+    doors = 4
+    windows = 3
+    seats = 2
+
+
+porche = Car() #porche는 Car의 인스턴스, instanciation
+
+print(porche.doors) #4
+
+
+# propery 추가
+porche.color = "RED"
+
+print(porche.color)
+
+ferrari = Car():
+ferrari.color="BLUE "
+
+
+
+class Car2():
+    wheels =4
+    doors = 4
+    windows = 3
+    seats = 2
+    def start(potato):
+        print(potato.color)
+        print("i start") #클래스 내부에 함수는 method
+
+        #외부에 있으면 함수
+
+
+
+porche = Car2()
+porche.color="RED COLOR"
+porceh.start()

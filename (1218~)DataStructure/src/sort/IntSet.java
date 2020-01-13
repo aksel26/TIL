8079f771460fe18a4e8927308be7e005c31cf75b
aksel26 <kevinxkim2023@gmail.com>
2020-01-13 17:14:27 +0900

    집합

--- (1218~)DataStructure/src/sort/IntSet.java
+++ (1218~)DataStructure/src/sort/IntSet.java
@@ -40,12 +40,7 @@ public class IntSet {
 
 	// 집합에 n 이 있는지 없는지 확인
 	public boolean contains(int n) {
-		if (num >= max || contains(n) == true) { // 가득 찼거나 이미 n이 존재.
-			return false;
-		} else {
-			set[num++] = n; // 가장 마지막 자리에 n 추가
-		}
-		return true;
+		return (indexOf(n)!=-1) ? true: false;
 	}
 
 	public boolean add(int n) {
@@ -56,6 +51,9 @@ public class IntSet {
 		}
 		return true;
 	}
+	
+	
+	
 	// 집합에서 n을 삭제한다.
 
 	public boolean remove(int n) {
@@ -72,7 +70,7 @@ public class IntSet {
 	// 집합 s에 복사한다.
 
 	public void copyTo(IntSet s) {
-		int n = (s.max < s.num) ? s.max : s.num; // 복사할 요소 개수
+		int n = (s.max < num) ? s.max : num; // 복사할 요소 개수
 		for (int i = 0; i < n; i++) {
 			s.set[i] = set[i];
 		}
@@ -81,7 +79,7 @@ public class IntSet {
 
 	// 집합 s를 복사한다.
 	public void copyFrom(IntSet s) {
-		int n = (max < s.num) ? s.max : s.num;
+		int n = (max < s.num) ? max : s.num;
 		for (int i = 0; i < n; i++) {
 			set[i] = s.set[i];
 		}
@@ -92,7 +90,7 @@ public class IntSet {
 //	집합 s와 같은지 확인
 
 	public boolean equalTo(IntSet s) {
-		if (num != s.num) {
+		if (num != s.num) { 	//요소개수가 같지 않으면 집합도 같지않다.
 			return false;
 
 		}
@@ -143,7 +141,51 @@ public class IntSet {
 	
 	
 	public static void main(String[] args) {
-
+		IntSet s1 = new IntSet(20);
+		IntSet s2 = new IntSet(20);
+		IntSet s3 = new IntSet(20);
+		
+		
+		s1.add(10);	// s1= {10}
+		s1.add(15); // s1= {10,15}
+		s1.add(20); // s1= {10,15,20}
+		s1.add(25);	// s1= {10,15,20,25}
+		
+		
+		s1.copyTo(s2);  //s2 ={10,15,20,25}
+		s2.add(12);		//s2 ={10,15,20,25,12}
+		s2.remove(25);	//s2 ={10,15,20,12}
+		
+		s3.copyFrom(s2); //s3 ={10,15,20,12}
+		
+		System.out.println("s1 = " +s1);
+		System.out.println("s2 = " + s2);
+		System.out.println("s3 = " + s3);
+		
+		
+		System.out.println("집합 s1에 있는 15는 " + (s1.contains(15)? "포함된다" :"포함되지 않는다"));
+		System.out.println("집합 s2에 있는 25는 " + (s2.contains(15)? "포함된다" :"포함되지 않는다"));
+		System.out.println("집합 s1과 s2는 " + (s1.equals(s2)? "같다" :"다르다"));
+		System.out.println("집합 s2와 s3은 " + (s2.equals(s3)? "같다" :"다르다"));
+		
+		s3.unionOf(s1, s2);
+		System.out.println("s3 = " + s3);
+		System.out.println("s1과 s2의 합집합은 " + s3 + "입니다.");
+		
 	}
 
 }
+
+//	출력
+
+//	s1 = {10 15 20 25 }
+//	s2 = {10 15 20 12 }
+//	s3 = {10 15 20 12 }
+//	집합 s1에 있는 15는 포함된다
+//	집합 s2에 있는 25는 포함된다
+//	집합 s1과 s2는 다르다
+//	집합 s2와 s3은 다르다
+//	s3 = {10 15 20 25 12 }
+//	s1과 s2의 합집합은 {10 15 20 25 12 }입니다.
+
+

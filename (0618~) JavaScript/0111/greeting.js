025d7efe6e73c37cccd4a8d8a2080c585571a613
aksel26 <kevinxkim2023@gmail.com>
2020-01-17 00:23:55 +0900

    시계

---
+++ (0618~) JavaScript/0111/greeting.js
@@ -0,0 +1,48 @@
+
+// 쿼리 셀렉터 : 원하는 셀렉터를 다 가져옴. 클래스, css.,,,
+//찾은 첫번째 것을 가져옴
+
+// 쿼리셀렉터올 : 모든걸 가져오는데 엘리먼테를 array로 가져옴
+
+// getElmentById
+
+
+//로컬에 저장하는 방법
+
+// 1. 개발자도구 : Application -> localStorage
+const form = document.querySelector(".js-form")
+
+    input = form.querySelector("input")
+
+    greeting = document.querySelector(".js-greetings")
+
+const USER_LS = "currentUser",
+SHOWING_CN ="showing";
+
+
+
+function paintGreeting(text){
+
+    console.log(text)
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `hello ${text}`;
+}
+
+function loadName(){
+    console.log(USER_LS)
+    const currentUser = localStorage.getItem(USER_LS);
+
+    if(currentUser ===null){
+      
+    }else{
+        paintGreeting(currentUser);
+    }
+}
+
+
+function init(){
+    loadName();
+}
+
+init()

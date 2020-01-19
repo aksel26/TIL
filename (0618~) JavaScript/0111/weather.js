dd054b823fcd21ee3f833e0cd6d19f5989ccc832
aksel26 <kevinxkim2023@gmail.com>
2020-01-20 00:26:14 +0900

    완료

--- (0618~) JavaScript/0111/weather.js
+++ (0618~) JavaScript/0111/weather.js
@@ -1,4 +1,20 @@
+const weather = document.querySelector(".js-weather")
+
 const COORDS = 'coords';
+const API_KEY = "a32603ee83cdacfc09784cdd78807beb"
+
+
+function getWeather(lat,lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`; 
+    })
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS,JSON.stringify(coordsObj))
@@ -13,6 +29,7 @@ function handleGeoSuccess(position){
     }
 
     saveCoords(coordsObj);
+    getWeather(latitude,longitude);
 }
 
 
@@ -29,7 +46,9 @@ function loadCoords(){
     if(loadedCoords === null){
         askForCoords();
     }else{
-
+        
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude,parsedCoords.longitude);
     }
 }
 function init(){

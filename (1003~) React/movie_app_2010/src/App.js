a59b6c9218561739670cec8eca29d4cf0552f35b
aksel26 <kevinxkim2023@gmail.com>
2020-01-20 10:01:45 +0900

    이미지 props

--- (1003~) React/movie_app_2010/src/App.js
+++ (1003~) React/movie_app_2010/src/App.js
@@ -2,30 +2,67 @@ import React from 'react';
 
 
 // props -> object -> fav
-function Food({fav}){
+function Food({name,image}){
   
 
   // object는 component로 전달된 모든 prop들
-  return <h1> i like {fav}</h1>
+  return (
+  
+    <div> 
+      <h1> I like {name}</h1>
+      <img src={image}/>
+    </div>
+  );
 }
+
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+  },
+  {
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+  },
+  {
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+];
+
+// JS - Map
+// array의 각 item에서 function을 실행하는 array를 가지는 javascript function이며
+// function의 result를 갖는 array를 반환
 
 
 
 
 function App() {
-  return  (<div>
-    <h1>Hello</h1>
-
+  return  (
+  <div>
     {/* Food Component에 kimchi라는 value로 prop name을 줌 */}
     {/* prop ; property */}
 
     {/* string, boolean, array로 정보를 보낼 수 있다. */}
-    <Food fav="Kimchi" />
-    <Food fav="ramen" />
-    <Food fav="pasta" />
-    <Food fav="Chicken" />
-
-    </div>
+    {/* dish는 object!! */}
+    {foodILike.map(dish => ( 
+      <Food name={dish.name} image={dish.image}/>
+      ))}
+  </div>
   );
  }
 

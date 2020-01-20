5188d1c4d3125f45023a3c5bb78aa5e6afc2d486
aksel26 <kevinxkim2023@gmail.com>
2020-01-20 09:03:02 +0900

    JSX & Props

--- (1003~) React/movie_app_2010/src/App.js
+++ (1003~) React/movie_app_2010/src/App.js
@@ -1,8 +1,40 @@
 import React from 'react';
 
 
+// props -> object -> fav
+function Food({fav}){
+  
+
+  // object는 component로 전달된 모든 prop들
+  return <h1> i like {fav}</h1>
+}
+
+
+
+
 function App() {
-  return  <div>hello!!!</div>;
-      }
+  return  (<div>
+    <h1>Hello</h1>
+
+    {/* Food Component에 kimchi라는 value로 prop name을 줌 */}
+    {/* prop ; property */}
+
+    {/* string, boolean, array로 정보를 보낼 수 있다. */}
+    <Food fav="Kimchi" />
+    <Food fav="ramen" />
+    <Food fav="pasta" />
+    <Food fav="Chicken" />
+
+    </div>
+  );
+ }
 
 export default App;
+
+
+// 1.  component는 HTML을 반환하는 함수
+// javascript와 HTML사이의 조합을 jsx라고 한다.
+
+
+// 2. component에 정보를 보낼 수 있다.
+//     children parent개념
--- (1003~) React/movie_app_2010/src/index.js
+++ (1003~) React/movie_app_2010/src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App"
 
 
+// 오류
+// import Potato from "./test"; 
+// ReactDOM.render(<App /><Potato/>, document.getElementById('root'));
+// 리액트 어플리케이션이 하나의 component만을 rendering해야하기 때문임.
+// 그 component가 App임. 
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 

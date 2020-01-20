adbbebb1a7aaccc17441fa083e2b0d1e56d95e38
aksel26 <kevinxkim2023@gmail.com>
2020-01-20 18:49:22 +0900

    Update App.js

--- (1003~) React/movie_app_2010/src/App.js
+++ (1003~) React/movie_app_2010/src/App.js
@@ -10,7 +10,7 @@ function Food({name,image}){
   
     <div> 
       <h1> I like {name}</h1>
-      <img src={image}/>
+      <img src={image} alt={name}/>
     </div>
   );
 }
@@ -18,26 +18,31 @@ function Food({name,image}){
 
 const foodILike = [
   {
+    id:1,
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
   },
   {
+    id:2,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
   },
   {
+    id:3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
   },
   {
+    id:4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
   },
   {
+    id:5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
@@ -60,7 +65,7 @@ function App() {
     {/* string, boolean, array로 정보를 보낼 수 있다. */}
     {/* dish는 object!! */}
     {foodILike.map(dish => ( 
-      <Food name={dish.name} image={dish.image}/>
+      <Food key={dish.id} name={dish.name} image={dish.image}/>
       ))}
   </div>
   );

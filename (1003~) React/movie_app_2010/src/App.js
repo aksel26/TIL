4617f46309a255c42310d4698c46696473bd83ad
aksel26 <kevinxkim2023@gmail.com>
2020-01-23 17:03:36 +0900

    api 가져오기

--- (1003~) React/movie_app_2010/src/App.js
+++ (1003~) React/movie_app_2010/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import PropTypes from "prop-types"
-
+import axios from "axios";
 
 
 class App extends React.Component{
   state = {
-    isLoading:true
-
+    isLoading:true,
+    movies : []
 
   };
 
+
+  getMovies= async() => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+
+  }
   componentDidMount(){
-    setTimeout(()=>{
-      this.setState({isLoading:false});
-    },2000);
+    this.getMovies();
+
+
+
   }
 
   

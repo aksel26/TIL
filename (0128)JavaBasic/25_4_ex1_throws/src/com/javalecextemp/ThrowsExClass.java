4360f3c0ed8f0da9606d7845d8445f88c9f16e95
aksel26 <kevinxkim2023@gmail.com>
2020-01-30 16:28:53 +0900

    arraylist

--- (0128)JavaBasic/25_4_ex1_throws/src/com/javalecextemp/ThrowsExClass.java
+++ (0128)JavaBasic/25_4_ex1_throws/src/com/javalecextemp/ThrowsExClass.java
@@ -1,16 +1,18 @@
 package com.javalecextemp;
 
 public class ThrowsExClass {
-	
-	public ThrowsExClass() {
-		actionC();
+		
+		public ThrowsExClass() { // 생성자
+		actionC(); // 생성자를 생성하자 마자 actionC를 실행
 	}
 	
 	private void actionA() throws Exception {
 		System.out.println("actionA");
 		
 		int[] iArr = {1, 2, 3, 4};
-		System.out.println(iArr[4]);
+		
+		System.out.println(iArr[4]); // 인덱스 값이 존재하지 않음 예외 발생 try catch를 해야하지만 throws를 해주었기 때문에
+									 //actionB로 간다. 
 		
 		System.out.println("actionAA");
 	}
@@ -21,13 +23,13 @@ public class ThrowsExClass {
 		try {
 			actionA();
 		} catch (Exception e) {
-			System.out.println("예외는 여기서 처리 할께요.^^");
+			System.out.println("예외는 여기서 처리 할께요.^^"); 
 			System.out.println(e.getMessage());
 		}
 		
 		System.out.println("actionBB");
 	}
-	
+	 
 	private void actionC() {
 		System.out.println("actionC");
 		actionB();
---
+++ (0128)JavaBasic/26_1_ex1_collection/src/com/javalecextemp/MainClass.java
@@ -0,0 +1,47 @@
+package com.javalecextemp;
+
+import java.util.ArrayList;
+
+public class MainClass {
+	public static void main(String[] args) {
+		
+		ArrayList<String> arrayList = new ArrayList<String>();
+		
+		arrayList.add("str0");
+		arrayList.add("str1");
+		arrayList.add("str2");
+		arrayList.add("str3");
+		
+		System.out.println(arrayList);
+		
+//		[str0, str1, str2, str3]
+		
+		arrayList.set(2, "str2222");
+		System.out.println(arrayList);
+		
+//		[str0, str1, str2222, str3]
+		
+		arrayList.remove(3);
+		System.out.println(arrayList);
+		System.out.println(arrayList.size()); 
+
+//		[str0, str1, str2222]
+//		3
+		
+		arrayList.clear();
+		System.out.println(arrayList);
+		
+//		[]
+
+		arrayList=null;
+//		System.out.println(arrayList.toString());
+		System.out.println(arrayList);
+		
+//		Exception in thread "main" java.lang.NullPointerException
+//		at com.javalecextemp.MainClass.main(MainClass.java:37)
+		
+//		null
+
+		
+	}
+}

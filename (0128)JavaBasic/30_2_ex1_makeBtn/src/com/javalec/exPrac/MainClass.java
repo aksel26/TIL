053a120148bfe9927750f48efd78eba02af08f10
aksel26 <kevinxkim2023@gmail.com>
2020-02-03 14:55:37 +0900

    makeBtn

--- (0128)JavaBasic/30_2_ex1_makeBtn/src/com/javalec/exPrac/MainClass.java
+++ (0128)JavaBasic/30_2_ex1_makeBtn/src/com/javalec/exPrac/MainClass.java
@@ -4,28 +4,26 @@ import java.awt.Dimension;
 
 public class MainClass {
 
-	
 	public static void main(String[] args) {
 		MakeBtn btn = new MakeBtn();
 //		btn.pack(); //버튼이 가질 수 있는 최소의 사이즈로 자동맞춤
-		btn.setSize(new Dimension(200,200));
+		btn.setSize(new Dimension(200, 200));
 		btn.setVisible(true);
-	try {
-		Thread.sleep(2000);
-	} catch (Exception e) {
-		
-		
-	}
-	btn.setVisible(false);
-	
-	btn.dispose(); //자원 해제
-	
-	try {
-		Thread.sleep(2000);
-	} catch (Exception e) {
-		// TODO: handle exception
-	}
-	
-	System.exit(0); //완전히 프로세스를 종
+		try {
+			Thread.sleep(2000);
+		} catch (Exception e) {
+
+		}
+		btn.setVisible(false);
+
+		btn.dispose(); // 자원 해제
+
+		try {
+			Thread.sleep(2000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		System.exit(0); // 완전히 프로세스를 종
 	}
 }
--- (0128)JavaBasic/30_2_ex1_makeBtn/src/com/javalec/exPrac/MakeBtn.java
+++ (0128)JavaBasic/30_2_ex1_makeBtn/src/com/javalec/exPrac/MakeBtn.java
@@ -3,11 +3,10 @@ package com.javalec.exPrac;
 import java.awt.Button;
 import java.awt.Frame;
 
-public class MakeBtn extends Frame{
+public class MakeBtn extends Frame {
 	public MakeBtn() {
 		Button btn = new Button("button");
 		add(btn);
 	}
-	
 
 }

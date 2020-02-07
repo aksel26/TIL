93fe1262d57fc7bb4b824ca8c601f49beed178ba
aksel26 <kevinxkim2023@gmail.com>
2020-02-07 22:14:42 +0900

    Update ComparatorEx.java

--- (1218~)DataStructure/src/comparable/ComparatorEx.java
+++ (1218~)DataStructure/src/comparable/ComparatorEx.java
@@ -27,7 +27,12 @@ class Descending implements Comparator {
 		if (o1 instanceof Comparable && o2 instanceof Comparable) {
 			Comparable c1 = (Comparable) o1;
 			Comparable c2 = (Comparable) o2;
-			return c1.compareTo(c2) * -1; // -1을 곱해서 기본 정렬방식의 역으로 변경한다.
+			
+			
+			System.out.println("c1:"+c1);
+			System.out.println("c2:"+c2);
+			System.out.println(c1.compareTo(c2)*-1);
+			return c1.compareTo(c2) * -1 ; // -1을 곱해서 기본 정렬방식의 역으로 변경한다.
 		}
 		return -1;
 	}

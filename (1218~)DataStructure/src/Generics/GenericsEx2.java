1a16185829badc91716b734fa308f3a8320d968f
aksel26 <kevinxkim2023@gmail.com>
2020-02-12 19:03:25 +0900

    generics, wildCard

---
+++ (1218~)DataStructure/src/Generics/GenericsEx2.java
@@ -0,0 +1,40 @@
+package Generics;
+
+import java.util.ArrayList;
+
+
+
+public class GenericsEx2 {
+	public static void main(String[] args) {
+		Box<Fruit> fruitBox = new Box<Fruit>();
+		Box<Apple> appleBox = new Box<Apple>();
+//		Box<Grape> grapeBox = new Box<Grape>();
+		Box<Toy> toy = new Box<Toy>();
+		
+		
+		fruitBox.add(new Fruit());
+		fruitBox.add(new Apple());
+		
+		appleBox.add(new Apple());
+		appleBox.add(new Apple());
+//		appleBox.add(new Toy());	//	Box<Apple>에는 Apple만 들어갈 수 있다.
+		
+		toy.add(new Toy());
+//		toy.add(new Apple());
+		
+		System.out.println(fruitBox);
+		System.out.println(appleBox);
+		System.out.println(toy);
+		
+		
+		
+		
+	}
+
+}
+
+
+
+//	[Fruit, Apple]
+//	[Apple, Apple]
+//	[Toy]

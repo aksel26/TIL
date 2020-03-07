8113e85dc0e24d83ccc1a9fe05234699d2570788
aksel26 <kevinxkim2023@gmail.com>
2020-03-08 02:47:57 +0900

    servletMapping

--- (0626~) Servlet/0626/Readme.md
+++ (0626~) Servlet/0626/Readme.md
@@ -2,9 +2,11 @@
 
 ## 중요 배경지식
 
-<img src="Readme.assets/image-20200308015544034.png" alt="image-20200308015544034" style="zoom:33%;" />
+
 
 <img src="Readme.assets/image-20200308010424090.png" alt="image-20200308010424090" style="zoom:33%;" />
+
+<img src="Readme.assets/image-20200308015544034.png" alt="image-20200308015544034" style="zoom:33%;" />
 
 - jsp파일은 반드시 WebContent아래에 있어야 한다.
 
@@ -14,4 +16,43 @@
 
 
 
+# Servlet 시작하기
+
 - jsp는 안에 html코드를 넣는거고 servlet은 순수 java파일을 이용하는 것.
+  - **jsp**는 <u>뷰</u> 부분
+  - **servlet** <u>controller, model</u> 서블렛을 이용
+
+## Servlet Mapping
+
+- <img src="Readme.assets/image-20200308022313100.png" alt="image-20200308022313100" style="zoom:33%;" />
+- servlet이 많아지면 구분이 필요하다 (중복불가)
+- full path를 보완한 mapping path
+
+
+
+### mapping을 어떻게 할까?
+
+- 방법1. web.xml을 이용하는 방법
+
+  ```xml
+    <servlet>
+    	<servlet-name>testServlet</servlet-name>
+    	<servlet-class>com.testPjt.testServlet</servlet-class>
+    </servlet>
+  
+  	<servlet-mapping>
+  		<servlet-name>testServlet</servlet-name>
+  		<url-pattern>/test</url-pattern>
+  	</servlet-mapping>
+  ```
+
+  - `servlet-name`은 위아래가 반드시 같아야한다.
+  - `servlet-class`는 패키지명.class이름까지 적어줘야 한다.
+
+- 방법2. annotation활용
+
+  `@WebServlet("/ts")`
+
+  
+
+  - test로도 매핑이 되고, ts로도 매핑이 되서 각각 모두 출력이 된다.
---
+++ (0626~) Servlet/0626/testPjt/src/com/testPjt/testServlet.java
@@ -0,0 +1,46 @@
+package com.testPjt;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class testServlet
+ */
+@WebServlet("/ts")
+public class testServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public testServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		
+		out.print("<h1>hello</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

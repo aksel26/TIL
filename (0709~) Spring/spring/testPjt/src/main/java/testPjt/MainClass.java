ac806818a1aad8f3edb4cbdc5042b696b55155cd
aksel26 <kevinxkim2023@gmail.com>
2020-04-01 16:19:31 +0900

    bean

--- (0709~) Spring/spring/testPjt/src/main/java/testPjt/MainClass.java
+++ (0709~) Spring/spring/testPjt/src/main/java/testPjt/MainClass.java
@@ -9,11 +9,12 @@ public class MainClass {
 		
 		
 //		xml파일에서 객체를 만든 후 사용
-		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml:"); //컨테이너를 가져옴
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml"); //컨테이너를 가져옴
 		
-		moveWalk mw = ctx.getBean("tWalk", moveWalk.class);
+		moveWalk mw = ctx.getBean("move", moveWalk.class);
+		
 		mw.move();
-		
+		 
 		ctx.close();
 		
 		

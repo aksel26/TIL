0354b760810942cafc9ecf30f76ef9d21c0d429a
aksel26 <kevinxkim2023@gmail.com>
2020-04-04 13:11:23 +0900

    sol6 ArrayList개념 복습

--- (20_0103)Algorithm/test/src/test/Solution6_1.java
+++ (20_0103)Algorithm/test/src/test/Solution6_1.java
@@ -8,15 +8,23 @@ public class Solution6_1 {
 	public static void main(String[] args) {
 		
 		
+//	ArrayList 개념 복습!
+		
 		ArrayList<Integer> al = new ArrayList<Integer>();
-		ArrayList<Integer> al2 = new ArrayList<Integer>();
+		
+		
+//		al에 요소 넣기
 		al.add(1);
 		al.add(2);
-		al.add(3);
+		al.add(3); 
+				
+		
+		ArrayList<Integer> al2 = new ArrayList<Integer>();
 		al2.add(4);
 		al2.add(5);
 		al2.add(6);
 		
+//		addAll메서드
 		al.addAll(al2);
 		for(int i : al) {
 			System.out.println(i);

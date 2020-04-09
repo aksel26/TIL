0add61ab596ab2b04156356295f005ed59dddf5d
aksel26 <kevinxkim2023@gmail.com>
2020-04-09 23:05:21 +0900

    콜라츠의 추측

---
+++ (20_0103)Algorithm/test/src/test/solution16/solution16.java
@@ -0,0 +1,94 @@
+package test.solution16;
+
+public class solution16 {
+	public int solution(int num) {
+
+		int answer = 0;
+		int cnt = 0;
+
+		while (num % 2 == 1) {
+			
+			if (num % 2 == 1) {
+
+				num = num * 3 + 1;
+				cnt++;
+				if (num != 1) {
+					while (num % 2 == 0) {
+
+						num = num / 2;
+						cnt++;
+
+						if (num % 2 == 1) {
+							
+							if( num ==1 ) {
+								System.out.println(num);
+								break;
+							}
+							
+						}
+
+					}
+				} 
+				
+
+			}
+		}
+		while (num % 2 == 0) {
+
+			if (num % 2 == 0) {
+
+				num = num / 2;
+
+				cnt++;
+
+				if (num != 1) {
+
+					if (num % 2 == 1) {
+						num = num * 3 + 1;
+						cnt++;
+						continue;
+					}
+
+				} else if (num == 1) {
+					answer = cnt;
+					break;
+				}
+			}
+
+		}
+		if (answer >= 105) {
+			answer = -1;
+
+		}
+		System.out.println(answer);
+
+//		System.out.println(answer);
+
+//		
+//		while (num % 2 == 0) {
+//			num = num / 2;
+//			cnt++;
+//
+//			if (num != 1) {
+//
+//				if (num % 2 == 1) {
+//					num = num * 3 + 1;
+//					cnt++;
+//					continue;
+//				}
+//
+//			} else if (num == 1) {
+//				answer = cnt;
+//				break;
+//			}
+//
+//		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		solution16 sol16 = new solution16();
+		sol16.solution(3);
+	}
+}

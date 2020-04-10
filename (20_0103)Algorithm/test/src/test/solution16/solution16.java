27cd9b7261edd50e49493c993ed27009c1cff3a6
aksel26 <kevinxkim2023@gmail.com>
2020-04-10 13:35:40 +0900

    콜라츠추측

--- (20_0103)Algorithm/test/src/test/solution16/solution16.java
+++ (20_0103)Algorithm/test/src/test/solution16/solution16.java
@@ -7,7 +7,7 @@ public class solution16 {
 		int cnt = 0;
 
 		while (num % 2 == 1) {
-			
+
 			if (num % 2 == 1) {
 
 				num = num * 3 + 1;
@@ -19,70 +19,19 @@ public class solution16 {
 						cnt++;
 
 						if (num % 2 == 1) {
-							
-							if( num ==1 ) {
-								
+
+							if (num == 1) {
+
 								break;
 							}
-							
+
 						}
 
 					}
-				} 
-				
-
-			}
-		}
-		while (num % 2 == 0) {
-
-			if (num % 2 == 0) {
-
-				num = num / 2;
-
-				cnt++;
-
-				if (num != 1) {
-
-					if (num % 2 == 1) {
-						num = num * 3 + 1;
-						cnt++;
-						continue;
-					}
-
-				} else if (num == 1) {
-					answer = cnt;
-					break;
 				}
+
 			}
-
 		}
-		if (answer >= 105) {
-			answer = -1;
-
-		}
-		System.out.println(answer);
-
-//		System.out.println(answer);
-
-//		
-//		while (num % 2 == 0) {
-//			num = num / 2;
-//			cnt++;
-//
-//			if (num != 1) {
-//
-//				if (num % 2 == 1) {
-//					num = num * 3 + 1;
-//					cnt++;
-//					continue;
-//				}
-//
-//			} else if (num == 1) {
-//				answer = cnt;
-//				break;
-//			}
-//
-//		}
 
 		return answer;
 	}
--- (20_0103)Algorithm/test/src/test/solution16/solution16_1.java
+++ (20_0103)Algorithm/test/src/test/solution16/solution16_1.java
@@ -2,17 +2,15 @@ package test.solution16;
 
 public class solution16_1 {
 	public int solution(int num) {
-	
-	
-	long n = (long)num ;
-	for(int i = 0 ;i < 500 ; i++) {
-		if(n==1) return i ;
-		n = (n % 2 == 0) ? n/2 :n *3 +1;
-	}
-	return -1;
-	
 
-		
+		long n = (long) num;
+		for (int i = 0; i < 500; i++) {
+			if (n == 1)
+				return i;
+			n = (n % 2 == 0) ? n / 2 : n * 3 + 1;
+		}
+		return -1;
+
 	}
 
 	public static void main(String[] args) {

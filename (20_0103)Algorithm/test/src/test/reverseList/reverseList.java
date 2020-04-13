a0173e909dda1f3b22cfcba234e41c6d77d46c94
aksel26 <kevinxkim2023@gmail.com>
2020-04-13 19:08:05 +0900

    뒤집기 재귀

---
+++ (20_0103)Algorithm/test/src/test/reverseList/reverseList.java
@@ -0,0 +1,19 @@
+package test.reverseList;
+
+import java.util.List;
+
+public class reverseList {
+
+	public static String flip(String n) {
+		 if ((null == n) || (n.length() <= 1)) {
+		        return n;
+		    }
+		return flip(n.substring(1))+n.charAt(0);
+	}
+
+	public static void main(String[] args) {
+		reverseList rl = new reverseList();
+		System.out.println(rl.flip("1234567"));
+	}
+
+}

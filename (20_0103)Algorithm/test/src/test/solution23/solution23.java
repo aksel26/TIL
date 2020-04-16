5f3e223bae1e00bd701b78b0bc0dec1691c990a3
aksel26 <kevinxkim2023@gmail.com>
2020-04-16 21:15:04 +0900

    자연수 뒤집어 배열로 만들기

---
+++ (20_0103)Algorithm/test/src/test/solution23/solution23.java
@@ -0,0 +1,40 @@
+package test.solution23;
+
+import java.util.*;
+
+public class solution23 {
+	public int[] solution(long n) {
+		int[] answer = {};
+
+		String str = "";
+		str = Long.toString(n); // String 자리수 split
+
+		StringBuffer sb = new StringBuffer(str);
+		sb.reverse();
+		String str2[] = null;
+		str2 = sb.toString().split("");
+
+
+		
+		// string[] -> int[] 배열에 넣기
+		
+		// 방법1. for문 사용
+		answer = new int[str2.length];
+		for (int i = 0; i < str2.length; i++) {
+			answer[i] = Integer.parseInt(str2[i]);
+		}
+		
+		// 방법2. stream 사용
+		answer = Arrays.stream(str2).mapToInt(Integer::parseInt).toArray();
+
+		
+		
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		solution23 sol23 = new solution23();
+		sol23.solution(12345);
+
+	}
+}

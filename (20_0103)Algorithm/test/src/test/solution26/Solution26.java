b642aadd339226b1637a6ed465e3261898c6368e
aksel26 <kevinxkim2023@gmail.com>
2020-04-18 13:33:17 +0900

    완주하지 못한 선수 HashMap

--- (20_0103)Algorithm/test/src/test/solution26/Solution26.java
+++ (20_0103)Algorithm/test/src/test/solution26/Solution26.java
@@ -3,23 +3,34 @@ package test.solution26;
 import java.util.*;
 
 public class Solution26 {
+	public String solution(String[] participant, String[] completion) {
+		String answer = "";
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		Map<String, Integer> map = new HashMap<>();
 
-		
-		for (int i = 0; i < b; i++) {
-			for (int j = 0; j < a; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
+
+		for (String arg : participant)
+			map.put(arg, map.getOrDefault(arg, 0) + 1);
+			System.out.println(map.get(2));
+		for (String arg : completion)
+			map.put(arg, map.get(arg) - 1);
 			
+		for (String key : map.keySet()) {
+			
+			if (map.get(key) != 0)
+				return key;
+
 		}
 		
-		
-		System.out.println(a + b);
+		return null;
+	}
 
+	public static void main(String[] args) {
+		Solution26 sol26 = new Solution26();
+
+		String participant[] = { "leo", "kiki", "eden" };
+		String completion[] = { "kiki", "eden" };
+
+		System.out.println(sol26.solution(participant, completion));
 	}
 }

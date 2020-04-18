84905bee0546b48600eef5b374092e21884bb477
aksel26 <kevinxkim2023@gmail.com>
2020-04-18 13:36:17 +0900

    완주하지 못한 선수 HashMap

--- (20_0103)Algorithm/test/src/test/solution26/Solution26.java
+++ (20_0103)Algorithm/test/src/test/solution26/Solution26.java
@@ -8,20 +8,22 @@ public class Solution26 {
 
 		Map<String, Integer> map = new HashMap<>();
 
-
+//		처음에는 leo, kiki, eden 이 다 map에 없으므로 0 + 1 = 1이 된다.
 		for (String arg : participant)
 			map.put(arg, map.getOrDefault(arg, 0) + 1);
-			System.out.println(map.get(2));
+
+//		두번째에서 kiki, eden에마 -1이 된다.
 		for (String arg : completion)
 			map.put(arg, map.get(arg) - 1);
-			
+
 		for (String key : map.keySet()) {
-			
+
+//		leo만 -1이 안되었으므로 key값이 1인 leo만 반환된다.
 			if (map.get(key) != 0)
 				return key;
 
 		}
-		
+
 		return null;
 	}
 

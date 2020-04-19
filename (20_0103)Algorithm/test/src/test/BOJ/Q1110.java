e0461d7189181ff76ea7704819f8398652b88aff
aksel26 <kevinxkim2023@gmail.com>
2020-04-19 19:35:23 +0900

    1110 BOJ

--- (20_0103)Algorithm/test/src/test/BOJ/Q1110.java
+++ (20_0103)Algorithm/test/src/test/BOJ/Q1110.java
@@ -9,55 +9,26 @@ public class Q1110 {
 		String rst = "";
 		String[] str = {};
 
-		
-		
-		while(!str.equals(num)) {
-		str = Integer.toString(num).split("");
+		while (!str.equals(num)) {
+			str = Integer.toString(num).split("");
 
-		int ten = Integer.parseInt(str[0]);
-		int one = Integer.parseInt(str[1]);
+			int ten = Integer.parseInt(str[0]);
+			int one = Integer.parseInt(str[1]);
 
-		int result = one + ten;
-		if(result<10){
-			rst = str[1] + Integer.toString(result);
-			
-		}else if(result>10) {
-			rst = str[1] + Integer.toString(result).charAt(1);
-			
+			int result = one + ten;
+			if (result < 10) {
+				rst = str[1] + Integer.toString(result);
+
+			} else if (result > 10) {
+				rst = str[1] + Integer.toString(result).charAt(1);
+
+			}
+
+			num = Integer.parseInt(rst);
+			System.out.println(num);
+
+			cnt++;
 		}
-		
-		num = Integer.parseInt(rst);
-		System.out.println(num);
-		
-		cnt++;
-		}
-		
-
-//		while (!str.equals(num)) {
-//			if (str.length() != 1) {
-//				int ten = str.charAt(0);
-//				int one = str.charAt(1);
-//				int result = one + ten;
-//				
-//				str = Integer.toString(one) + Integer.toString(result).charAt(1);
-//				cnt++;
-//				break;
-//			} else if (str.length() == 1) {
-//				
-//				str = "0" + str.charAt(0);
-//
-//				char ten = str.charAt(0);
-//				int iten = (int)(ten);
-//				
-//				char one = str.charAt(1);
-//				int result = (int)(one + ten);
-////				System.out.println(result);
-//				
-//				str = Integer.toString(one) + Integer.toString(result).charAt(1);
-//				cnt++;
-//				break;
-//			}
-//		}
 
 		return cnt;
 	}

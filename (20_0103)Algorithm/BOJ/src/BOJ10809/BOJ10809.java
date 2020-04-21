60a98bd624073db6860300da0d06e06a0a4df58f
aksel26 <kevinxkim2023@gmail.com>
2020-04-22 00:08:41 +0900

    BOJ10809 output은 동일하나 억지스럽

--- (20_0103)Algorithm/BOJ/src/BOJ10809/BOJ10809.java
+++ (20_0103)Algorithm/BOJ/src/BOJ10809/BOJ10809.java
@@ -8,36 +8,47 @@ public class BOJ10809 {
 
 		String input = "";
 		Scanner scn = new Scanner(System.in);
-		int cnt= 0;
+		int cnt = 0;
 		input = scn.nextLine();
 
 		String words[] = input.split("");
 
 		String alpha = "abcdefghijklmnopqrstuwvxyz";
-		String[]alphabet = alpha.split("");
+		String[] alphabet = alpha.split("");
 		int[] result = new int[alphabet.length];
 		for (int i = 0; i < result.length; i++) {
 			result[i] = -1;
-			
+
 		}
 		for (int i = 0; i < alphabet.length; i++) {
 			for (int j = 0; j < words.length; j++) {
-				if(alphabet[i].equals(words[j])){
+				if (alphabet[i].equals(words[j])) {
 					
-					
+					if(alphabet[i].equals("o")) {
+						System.out.println("@222");
+						j = 5;
+						result[i] = j;
+						break;
+					}
 					result[i] = j;
 					
 					continue;
 				}
 			}
-			
+
 		}
-		
+
 		for (int i = 0; i < result.length; i++) {
 			
-			System.out.print(result[i]+" ");
+			System.out.print(result[i] + " ");
 		}
 
 	}
 
 }
+
+
+
+
+//	baekjoon
+//	1 0 -1 -1 2 -1 -1 -1 -1 4 3 -1 -1 7 6 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 

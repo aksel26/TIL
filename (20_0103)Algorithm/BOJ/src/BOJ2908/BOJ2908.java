dc4de4ad4a58fb66957197823431c2197a477217
aksel26 <kevinxkim2023@gmail.com>
2020-04-23 02:00:00 +0900

    BOJ2908

--- (20_0103)Algorithm/BOJ/src/BOJ2908/BOJ2908.java
+++ (20_0103)Algorithm/BOJ/src/BOJ2908/BOJ2908.java
@@ -5,31 +5,15 @@ import java.util.Scanner;
 public class BOJ2908 {
 
 	public static void main(String[] args) {
+		
 		Scanner scn = new Scanner(System.in);
-		String result = "";
-		String result2 = "";
-
-		String[] num1 = scn.nextLine().split("");
-		String[] num2 = scn.nextLine().split("");
-
-		String temp = num1[num1.length - 1];
-		num1[num1.length - 1] = num1[0];
-		num1[0] = temp;
-		for (int i = 0; i < num1.length; i++) {
-			result += num1[i];
+		
+		int height[] = new int[9];
+		
+		for (int i = 0; i < height.length; i++) {
+			height[i] =scn.nextInt();	
+			System.out.println(height[i]);
 		}
-		int res = Integer.parseInt(result);
-		String temp2 = num2[num2.length - 1];
-		num2[num2.length - 1] = num2[0];
-		num2[0] = temp2;
-		for (int i = 0; i < num2.length; i++) {
-			result2 += num2[i];
-		}
-
-		int res2 = Integer.parseInt(result2);
-
-		System.out.println(Math.max(res, res2));
-
+				
 	}
-
 }

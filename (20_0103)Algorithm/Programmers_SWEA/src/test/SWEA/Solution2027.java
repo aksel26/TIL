e6821c895486f0ec774668b251e719335c3b12b5
aksel26 <kevinxkim2023@gmail.com>
2020-04-24 17:48:18 +0900

    2027

---
+++ (20_0103)Algorithm/Programmers_SWEA/src/test/SWEA/Solution2027.java
@@ -0,0 +1,14 @@
+package test.SWEA;
+
+import java.util.Scanner;
+
+class Solution2027 {
+	public static void main(String args[]) throws Exception {
+		System.out.println("#++++");
+		System.out.println("+#+++");
+		System.out.println("++#++");
+		System.out.println("+++#+");
+		System.out.println("++++#");
+
+	}
+}

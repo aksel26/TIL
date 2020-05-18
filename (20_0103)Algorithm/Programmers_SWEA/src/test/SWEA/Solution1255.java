8819327e95a77a0c706447ac4c0a5c1bc30045b8
aksel26 <kevinxkim2023@gmail.com>
2020-05-18 16:56:13 +0900

    Solution1255 암호찾기

--- (20_0103)Algorithm/Programmers_SWEA/src/test/SWEA/Solution1255.java
+++ (20_0103)Algorithm/Programmers_SWEA/src/test/SWEA/Solution1255.java
@@ -1,5 +1,7 @@
 package test.SWEA;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Solution1255 {
@@ -7,31 +9,48 @@ public class Solution1255 {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 
-		int T = scn.nextInt();
+		
 
-		int[] array = new int[8];
+		
 
-		for (int i = 0; i < T; i++) {
+		Queue<Integer> que = new LinkedList<Integer>();
 
-			
+
+		for (int i = 0; i < 10; i++) {
+			int temp = 1;
+			int T = scn.nextInt();
 //			배열 넣기
-			for (int j = 0; j < array.length; j++) {
-				array[j] = scn.nextInt();
+			for (int j = 1; j < 9; j++) {
+				que.add(scn.nextInt());
+			}
 
-			}
-			
 //			수 빼기 사이클
-			for (int j = 0; j < array.length; j++) {
-				int endNum = array[0] - 1;
+			while(temp!=0) {
 				
-				array[j] = array[j+1];
-				
-				array[0] = endNum;
+				for (int j = 1; j < 6; j++) {
+					
+					temp = que.poll();
+					temp = temp - j;
+					
+					if (temp < 0) {
+						temp = 0;
+					}
+					
+					que.add(temp);
+					if (temp == 0) {
+						break;
+					}
+				}
 			}
+
+			System.out.print("#" + T + " ");
 			
-			for (int j = 0; j < array.length; j++) {
-				System.out.print(array[j]+" ");
+			
+			for (int j = 0; j < 8; j++) {
+				temp = que.poll();
+				System.out.print(temp+ " ");
 			}
+			System.out.println();
 
 		}
 	}

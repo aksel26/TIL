b3a2531665c2fd8ff10f60904e1377ce66cf52e4
aksel26 <kevinxkim2023@gmail.com>
2020-05-27 15:03:25 +0900

    배열 역순 출력

--- 200520_C/vs/ArrayStudy.c
+++ 200520_C/vs/ArrayStudy.c
@@ -5,7 +5,30 @@
 
 int main(void)
 {
-    char a[10]= "SEOUL";
-    printf("%s\n", a);
+    // 배열의 역순 출력
+    int size, i, num;
+    scanf("%d", &size);
+    int a[size];
+
+    for (i = 0; i < size; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = size - 1; i >= 0; i--)
+    {
+        printf("%d", a[i]);
+    }
+    // 입력
+    // 7
+    // 6 3 7 8 4 9 5
+    // 출력
+    // 5948736
+
+
+    // 배열의 회전
+    
+
+
     return 0;
 }

25034c9154054a225ced58f8cc35397e08a4bf63
aksel26 <kevinxkim2023@gmail.com>
2020-05-27 15:19:59 +0900

    배열의 회전

--- 200520_C/vs/ArrayStudy.c
+++ 200520_C/vs/ArrayStudy.c
@@ -6,29 +6,56 @@
 int main(void)
 {
     // 배열의 역순 출력
-    int size, i, num;
-    scanf("%d", &size);
-    int a[size];
+    // int size, i, num;
+    // scanf("%d", &size);
+    // int a[size];
 
-    for (i = 0; i < size; i++)
-    {
-        scanf("%d", &a[i]);
-    }
+    // for (i = 0; i < size; i++)
+    // {
+    //     scanf("%d", &a[i]);
+    // }
 
-    for (i = size - 1; i >= 0; i--)
-    {
-        printf("%d", a[i]);
-    }
+    // for (i = size - 1; i >= 0; i--)
+    // {
+    //     printf("%d", a[i]);
+    // }
     // 입력
     // 7
     // 6 3 7 8 4 9 5
     // 출력
     // 5948736
 
-
     // 배열의 회전
-    
+    // int a[101], rotate, tmp,j ,i;
 
+    // for (i = 0; i < 9; i++)
+    // {
+    //     scanf("%d", &a[i]);
+    // }
+    // scanf("%d", &rotate);
+    // for (i = 0; i < rotate; i++)
+    // {
+    //     tmp = a[0];
+    //     a[0] = a[9];
+    //     a[9] = tmp;
+        
+    //     for ( j = 0; j < 9; j++)
+    //     {
+    //         a[j] = a[j + 1];    
+    //     }
+        
+        
+    // }
+    // for ( i = 0; i < 9; i++)
+    // {
+    //     printf("%d ",a[i]);
+    // }
+    // 입력
+    // 5 9 6 11 33 21 66 15 35
+    // 3(회전수)
+    // 출력
+    // 11 33 21 66 15 35 5 9 6 
+    
 
     return 0;
 }

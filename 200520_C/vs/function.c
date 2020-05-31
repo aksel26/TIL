cf98c048c1a1b1de21f5700e2eb57a0c0ad11d7e
aksel26 <kevinxkim2023@gmail.com>
2020-05-31 20:58:24 +0900

    함수로 별 찍기

--- 200520_C/vs/function.c
+++ 200520_C/vs/function.c
@@ -3,10 +3,30 @@
 
 // 보통 함수를 main위에다가 정의하지만,
 // 밑에다가 정의할 경우 위에 이렇게 꼭 명시해 주어야한다.
+
+void prntStar(int n ){
+    int i, j;
+
+    for ( i = 0; i < n; i++)
+    {
+        for ( j = 0; j <=  i; j++)
+        {
+            printf("*");
+        }
+        printf("\n")   ;
+    }
+    
+}
+
+
 int abs (int x);
 
 
 int main (){
+    int n;
+    scanf("%d", &n);
+    prntStar(n);
+
     int a; 
     a = -3;
     printf("%d\n", abs(a));
@@ -19,3 +39,5 @@ int abs(int x){
     if( x< 0) return x* -1;
     else return x;
 }
+
+

9892c79f1f1853cff05434c9eac9cdfc9fc18844
aksel26 <kevinxkim2023@gmail.com>
2020-06-18 15:23:59 +0900

    Namespace

---
+++ 200615_C++/io/io.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+
+
+    int number;
+    char letter;
+    float rnumber;
+
+
+    cout<<"정수 문자 실수 순서로 입력하세요 : " ;
+    cin >> number >> letter >> rnumber;
+    cout << "당신이 입력하 정수는 ?" <<number <<"입니다." <<endl;
+    cout << "당신이 입력하 문자는 ?" <<letter <<"입니다." <<endl;
+    cout << "당신이 입력하 실수는 ?" <<rnumber <<"입니다." <<endl;
+
+
+    return 0 ;
+
+}

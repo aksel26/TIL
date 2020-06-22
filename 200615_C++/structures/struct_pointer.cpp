8243bb290e7081c399cfa4bebbd2be8653518ac2
aksel26 <kevinxkim2023@gmail.com>
2020-06-22 19:28:46 +0900

    Update struct_pointer.cpp

--- 200615_C++/structures/struct_pointer.cpp
+++ 200615_C++/structures/struct_pointer.cpp
@@ -1,54 +1,48 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <cstring>
 
 using namespace std;
 #define MAX 10
 
-int main(){
+int main()
+{
 
     struct meminfo
     {
         char name[20];
         int salary;
-
     };
 
-    meminfo person[MAX]={
+    meminfo person[MAX] = {
         {"김대표", 100000},
         {"이부장", 70000},
         {"홍과장", 40000},
 
     };
 
-    meminfo* ptr;
-    
+    meminfo *ptr;
 
     ptr = person;
-    
+
     int count = 3;
 
-    
-    for ( ; count < MAX; count++)
+    for (; count < MAX; count++)
     {
-        cout<<"이름은 ? ";
-        cin>>(ptr+count)->name;
+        cout << "이름은 ? ";
+        cin >> (ptr + count)->name;
         // 프로그램 종료
-        if(strcmp((ptr+count)->name,"end")==0)
+        if (strcmp((ptr + count)->name, "end") == 0)
             break;
-        cout<<"연봉은 ? ";
-        cin>>(ptr+count)->salary;
-        
-        
+        cout << "연봉은 ? ";
+        cin >> (ptr + count)->salary;
     }
 
-    for ( count--;count>=0; count--)
+    for (count--; count >= 0; count--)
     {
-        cout<< (ptr+count)->name <<"의 연봉은 "
-            <<(ptr+count)->salary<<"입니다 "<<endl;
-
+        cout << (ptr + count)->name << "의 연봉은 "
+             << (ptr + count)->salary << "입니다 " << endl;
     }
 
+    //출력 시 count변수에 마지막에 입력된 원소의 위치가 저장되어 있으므로 마지막 원소부터 첫번째 원소의 순서로 출력돔.
     return 0;
-        
-    
 }

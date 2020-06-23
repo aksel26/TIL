ac73aa4bdf0b2d5d2ae1deb079c4b8f227c35c10
aksel26 <kevinxkim2023@gmail.com>
2020-06-23 15:21:21 +0900

    함수 call by value, referenct

---
+++ 200615_C++/func/ref.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+// call by reference
+void swap(int &, int &);
+
+int main()
+{
+
+    int x, y;
+
+    cout << "두 수를 입력하세요" << endl;
+
+    cin >> x >> y;
+
+    //함수 호출
+    swap(x, y);
+
+    cout << "main : x = " << x << " y = " << y << endl;
+
+    return 0;
+}
+
+// 함수 정의
+
+void swap(int &x, int &y)
+{
+
+    int tmp;
+
+    tmp = x;
+    x = y;
+    y = tmp;
+    // swap함수가 소환되어 먼저 출력된다.
+    cout << "swap : x = " << x << " y = " << y << endl;
+}
+
+// 입력
+// 4 6
+// 출력
+// swap : x = 6 y = 4
+// main : x = 6 y = 4
--- 200615_C++/union/unionVar.cpp
+++ 200615_C++/union/unionVar.cpp
@@ -2,29 +2,24 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-    union KEY{
-    // 전체 크기는 4바이트로 전체는 input멤버로 각 한바이트마다 byte배열 멤버로 접근
+    union KEY {
+        // 전체 크기는 4바이트로 전체는 input멤버로 각 한바이트마다 byte배열 멤버로 접근
         unsigned int input;
         unsigned char byte[4];
-
     };
-
 
     KEY key;
     key.input = 0xc0a86401;
 
-    int i ;
+    int i;
 
-    for ( i = 3; i >= 0; i--)
+    for (i = 3; i >= 0; i--)
     {
-        cout<<(int)key.byte[i]<<endl;
-
+        cout << (int)key.byte[i] << endl;
     }
 
     return 0;
-    
-
-
 }

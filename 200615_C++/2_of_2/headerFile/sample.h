3b10fe486e638d4a78db039681ab399ff5ba4cc9
aksel26 <kevinxkim2023@gmail.com>
2020-06-24 22:30:19 +0900

    헤더활용

---
+++ 200615_C++/2_of_2/headerFile/sample.h
@@ -0,0 +1,12 @@
+#ifndef _SAMPLE_H
+#define _SAMPLE_H
+
+struct type
+{
+    int x;
+    int y;
+};
+
+
+
+#endif
---
+++ 200615_C++/2_of_2/headerFile/source.cpp
@@ -0,0 +1,16 @@
+#include "sample.h"
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+
+    type t;
+    t.x = 10;
+    t.y = 20;
+
+    cout <<"t구조체의 x = " << t.x <<"\nt구조체의 y = "<< t.y<<endl;
+
+    return 0;
+}

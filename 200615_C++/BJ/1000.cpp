5724d9cc015648f1c6897dd44f3e6d84dfc38d0f
aksel26 <kevinxkim2023@gmail.com>
2020-07-01 21:42:41 +0900

    BJ

---
+++ 200615_C++/BJ/1000.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a,b,result;
+
+    cin>>a>>b;
+
+    result = a + b;
+
+    cout<<result<<endl;
+
+
+
+    return 0;
+}
---
+++ 200615_C++/BJ/1001.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    double result, a, b;
+
+    cin >> a >> b;
+
+    cout.precision(15);
+
+    cout << a/b;
+
+    return 0;
+}

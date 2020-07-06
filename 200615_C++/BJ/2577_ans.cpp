ca275aa3cb8b51fdef5f736f6dd08586b65049b7
aksel26 <kevinxkim2023@gmail.com>
2020-07-06 20:13:51 +0900

    BJ2577

--- 200615_C++/BJ/2577_ans.cpp
+++ 200615_C++/BJ/2577_ans.cpp
@@ -4,26 +4,24 @@ using namespace std;
 int arr[10];
 int main()
 {
-    int a, b , c, result= 0;
+    int a, b, c, result = 0;
 
-    cin>>a>>b>>c;
+    cin >> a >> b >> c;
 
-    result = a*b*c;
+    result = a * b * c;
 
-    while (result !=0)  
+    while (result != 0)
     {
-        arr[result%10] +=1;
-        cout<<"result%10 = "<<result %10<<endl;
+        cout << "result%10 = " << result % 10 << endl;
+        arr[result % 10] += 1;
+        cout << "result /= 10; = " << result / 10 << endl;
         result /= 10;
-        cout<<"result /= 10; = "<<result/10<<endl;
     }
 
     for (int i = 0; i < 10; i++)
     {
-        cout<<arr[i]<<endl;
+        cout << arr[i] << endl;
     }
-    
-    
-    
+
     return 0;
 }

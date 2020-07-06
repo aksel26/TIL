0deb0c7ebaa9c341457baa20a8e9fab4d6dfef2f
aksel26 <kevinxkim2023@gmail.com>
2020-07-06 17:13:15 +0900

    BJ2562

---
+++ 200615_C++/BJ/2562.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main()
+{
+
+    int size, max = -100000, idx = 0;
+    
+    int array1[9];
+    for (int i = 0; i < 9; i++)
+    {
+        cin >> array1[i];
+
+        if (max < array1[i])
+        {
+            max = array1[i];
+            idx = i+1;
+        }
+    }
+
+    cout << max << "\n"
+         << idx << endl;
+}

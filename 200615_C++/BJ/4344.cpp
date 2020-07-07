8b7773063c1f85cc6a454e7e359b2f9e8c4fa654
aksel26 <kevinxkim2023@gmail.com>
2020-07-07 12:42:57 +0900

    BJ4344

--- 200615_C++/BJ/4344.cpp
+++ 200615_C++/BJ/4344.cpp
@@ -6,12 +6,15 @@ int main()
 
     int tcase, size, total, avg;
     double cnt, result;
+    //test케이스
     cin >> tcase;
 
     for (int i = 0; i < tcase; i++)
     {
         cin >> size;
         int arr[size];
+
+        //평균 구하기
         for (int i = 0; i < size; i++)
         {
 
@@ -20,6 +23,8 @@ int main()
         }
         avg = total / size;
 
+
+        // 평균 미달인 점수의 갯수
         for (int i = 0; i < size; i++)
         {
             if (avg < arr[i])
@@ -27,10 +32,32 @@ int main()
                 cnt++;
             }
         }
+
+        // 평균 미달 비율
         result = cnt / size * 100;
+
+        // 소숫점 표현
         cout << fixed;
-        cout << result << endl;
+        cout.precision(3);
+        cout << result << "%" << endl;
+        total = 0, cnt = 0;
     }
 
     return 0;
 }
+
+
+// 입력
+// 5 (tcase)
+// 5 50 50 70 80 100
+// 7 100 95 90 80 70 60 50
+// 3 70 90 80
+// 3 70 90 81
+// 9 100 99 98 97 96 95 94 93 91
+
+// 출력
+// 40.000%
+// 57.143%
+// 33.333%
+// 66.667%
+// 55.556%

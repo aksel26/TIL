4410f7965f6421f2e4e027863452b6d2118f1a57
aksel26 <kevinxkim2023@gmail.com>
2020-07-08 18:27:04 +0900

    BJ1157_미완..

---
+++ 200615_C++/BJ/1157/1157.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+
+    int cnt = 0;
+    string word;
+    string result;
+    int asc = 0;
+    int arr[127] = {0};
+
+    cin >> word;
+
+    for (int i = 0; i < word.length(); i++)
+    {
+        asc = word[i];
+        if (asc >= 97 || asc <= 122)
+        {
+            asc = asc - 32;
+            arr[asc]++;
+        }
+        else
+        {
+            arr[asc]++;
+        }
+    }
+
+    int max = arr[0];
+
+    for (int i = 65; i <= 90; i++)
+    {
+        if (max < arr[i])
+        {
+            max = arr[i];
+            while (1)
+            {
+
+                if (max == arr[i + 1])
+                {
+                    cout << "?" << endl;
+                    break;
+                }
+            };
+            cnt = i;
+        }
+    }
+
+    result = cnt;
+
+    cout << result << endl;
+
+    return 0;
+}

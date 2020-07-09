762b67a535d755f8e984e0ed65e532bf6aa4128b
aksel26 <kevinxkim2023@gmail.com>
2020-07-09 13:42:59 +0900

    BJ8392

---
+++ 200615_C++/BJ/8392.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int tcase;
+    int a, sum = 0;
+    cin >> a;
+    for (int i = 0; i <= a; i++)
+    {
+        sum += i;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}

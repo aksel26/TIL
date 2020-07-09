8b6063dd01ae70ee54596baf849bfb79b037c71b
aksel26 <kevinxkim2023@gmail.com>
2020-07-09 13:42:54 +0900

    BJ1000

---
+++ 200615_C++/BJ/1000.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a, b, result;
+
+    cin >> a >> b;
+
+    result = a + b;
+
+    cout << result << endl;
+
+    return 0;
+}

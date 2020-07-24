e8e729f0adf41f719071d1bad319136c923ab457
aksel26 <kevinxkim2023@gmail.com>
2020-07-25 00:23:26 +0900

    BJ5596 정리

--- 200615_C++/BJ/5596/5596.cpp
+++ 200615_C++/BJ/5596/5596.cpp
@@ -6,31 +6,33 @@ int main()
 {
 
     int a, b, c, d;
-    
+
     int score, totalA = 0, totalB = 0;
 
     for (int i = 0; i < 4; i++)
     {
-        cin>>score;
+        cin >> score;
         totalA += score;
     }
-    
+
     for (int i = 0; i < 4; i++)
     {
-        cin>>score;
+        cin >> score;
         totalB += score;
     }
 
-    if(totalA > totalB){
-        cout<<totalA<<endl;
-    }else if(totalA<totalB){
-        cout<<totalB<<endl;
-    }else if(totalA == totalB){
-        cout<<totalA<<endl;
+    if (totalA > totalB)
+    {
+        cout << totalA << endl;
     }
-
-
-    
+    else if (totalA < totalB)
+    {
+        cout << totalB << endl;
+    }
+    else if (totalA == totalB)
+    {
+        cout << totalA << endl;
+    }
 
     return 0;
 }

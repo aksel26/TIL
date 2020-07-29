2d871cfcc7f09bf1eacafb49c445f02df55cf586
aksel26 <kevinxkim2023@gmail.com>
2020-07-29 15:51:16 +0900

    Insert Sort

---
+++ (20_0103)Algorithm/Sort/Insert/insert.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int i, j, temp;
+
+    int arr[10] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+
+    for (int i = 0; i < 9; i++)
+    {
+        j = i;
+        while (arr[j] > arr[j + 1])
+        {
+            temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+            j--;
+            if(j<0) break;
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}

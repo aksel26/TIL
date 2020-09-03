2d6bcda513ab91804692dc095cf94816de8f0225
aksel26 <kevinxkim2023@gmail.com>
2020-09-03 16:05:20 +0900

    JS json

---
+++ (0618~) JavaScript/0729_APIs/json.js
@@ -0,0 +1,9 @@
+// 1. Object to JSON
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple','banana']);
+console.log(json);
+
+json = JSON.stringify(['apple','banana']);
+// 2. JSON to Object

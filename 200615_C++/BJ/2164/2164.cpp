681a85003523449f629c9ebcc7dc99f706142161
aksel26 <kevinxkim2023@gmail.com>
2020-09-26 16:19:36 +0900

    카드2 미완

---
+++ 200615_C++/BJ/2164/2164.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    queue<int> q;
+    for (int i = 1; i <= n; i++)
+    {
+        q.push(i);
+    }
+    int size = q.size();
+    
+    cout<<size<<endl;
+    while (size != 1)
+    {
+
+        q.pop();
+        int temp = q.front();
+
+        q.pop();
+
+        q.push(temp);
+    }
+
+    cout << q.front() << endl;
+
+    return 0;
+}

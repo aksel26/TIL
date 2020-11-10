1d3f4c86e61df2c0258253b0e1ee7f5fa3e9fc8d
aksel26 <kevinxkim2023@gmail.com>
2020-11-10 20:27:11 +0900

    57번

---
+++ (20_0103)Algorithm/Lecture/57.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// void rec(int x)
+// {
+//     if (x == 0)
+//         return;
+//     else
+//     {
+//         rec(x / 2);
+//         printf("%d ", x % 2);
+//     }
+// }
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    // rec(n);
+
+    printf("%d",5 % 2);
+    return 0;
+}

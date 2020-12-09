4c02b435edbf66b5fd842ac47d2bc93e3a426676
aksel26 <kevinxkim2023@gmail.com>
2020-12-09 15:49:16 +0900

    최대 최소 힙

---
+++ (20_0103)Algorithm/Lecture/74.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    priority_queue<int> pQ;
+    while (true)
+    {
+        scanf("%d", &a);
+
+        if (a == -1)
+            break;
+
+        if (a == 0)
+        {
+            if (pQ.empty())
+                printf("-1\n");
+            else
+            {
+                printf("%d\n", -pQ.top());
+                pQ.pop();
+            }
+        }
+        else
+            pQ.push(-a);
+    }
+    return 0;
+}

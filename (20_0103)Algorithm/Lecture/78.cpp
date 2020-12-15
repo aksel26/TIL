7dc50db0098a887f40ab7430df0195aa5a77c48c
aksel26 <kevinxkim2023@gmail.com>
2020-12-15 18:37:00 +0900

    원더랜드

---
+++ (20_0103)Algorithm/Lecture/78.cpp
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int r[101];
+
+struct Edge
+{
+    int v1;
+    int v2;
+    int val;
+
+    Edge(int a, int b, int c)
+    {
+        v1 = a;
+        v2 = b;
+        val = c;
+    }
+
+    bool operator<(const Edge &b) const
+    {
+        return val < b.val;
+    }
+};
+int find(int v)
+{
+    if (v == r[v])
+    {
+        return v;
+    }
+    else
+        return r[v] = find(r[v]);
+}
+
+void Union(int a, int b)
+{
+    a = find(a);
+    b = find(b);
+    if (a != b)
+        r[a] = b;
+}
+int main()
+{
+
+    int a, b, c, v, e, i, res = 0;
+    vector<Edge> Ve;
+    scanf("%d %d", &v, &e);
+
+    for (i = 1; i <= v; i++)
+    {
+        r[i] = i;
+    }
+
+    for (i = 1; i <= e; i++)
+    {
+        scanf("%d %d %d", &a, &b, &c);
+        Ve.push_back(Edge(a, b, c));
+    }
+
+    sort(Ve.begin(), Ve.end());
+
+    for (i = 0; i < e; i++)
+    {
+        int fa = find(Ve[i].v1);
+        int fb = find(Ve[i].v2);
+        if (fa != fb)
+        {
+            res += Ve[i].val;
+            Union(Ve[i].v1, Ve[i].v2);
+        }
+    }
+
+    printf("\n%d\n", res);
+
+    return 0;
+}

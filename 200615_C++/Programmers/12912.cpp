9b946a487e76fb85d9b5ce5a14c611d39ba28e17
aksel26 <kevinxkim2023@gmail.com>
2020-12-26 18:19:56 +0900

    두 정수 사이의 합

---
+++ 200615_C++/Programmers/12912.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b)
+{
+    long long answer = 0;
+    int i, tmp;
+    if (a > b)
+    {
+        tmp = b;
+        b = a;
+        a = tmp;
+    }
+    for (i = a; i <= b; i++)
+    {
+        answer += i;
+    }
+
+    printf("%d\n", answer);
+
+    return answer;
+}
+
+int main()
+{
+
+    int a, b;
+    a = 5, b = 3;
+    solution(a, b);
+    return 0;
+}

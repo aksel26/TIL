35560514ad9fbb0f692c2098fbf8cec34ae3b38c
김현민 <kevinxkim2023@gmail.com>
2021-01-22 02:27:33 +0900

    BOJ9012

--- 200615_C++/BJ/9012/9012.cpp
+++ 200615_C++/BJ/9012/9012.cpp
@@ -5,23 +5,40 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    stack<char> st;
 
     int n;
-
+    cin >> n;
     string str;
     for (int i = 0; i < n; i++)
     {
         cin >> str;
-
-        while (1)
+        stack<char> st;
+        bool flag = false;
+        for (int j = 0; j < str.size(); j++)
         {
-            for (int i = 0; i < count; i++)
+            char temp = str[j];
+            if (temp == '(')
             {
-                /* code */
+                st.push(temp);
             }
+            else if (temp == ')')
+            {
+                if (st.empty() || st.top() != '(')
+                {
+                    flag = true;
+                    break;
+                }
+                st.pop();
+            }
+        }
 
-            if (str[i])
+        if (flag == true || !st.empty())
+        {
+            cout << "NO" << endl;
+        }
+        else if (st.empty())
+        {
+            cout << "YES" << endl;
         }
     }
 

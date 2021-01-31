d55e095c6d26f9b3844e3f55d5009d9be1917044
김현민 <kevinxkim2023@gmail.com>
2021-01-31 17:27:01 +0900

    BOJ1654

---
+++ 200615_C++/BJ/1654/1654.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<int> v;
+int main(int argc, char const *argv[])
+{
+    long n, k, ans = 0;
+    long long maxVal = 0, left = 0, mid = 0, right = 0;
+    int nums;
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums;
+        v.push_back(nums);
+        if (maxVal < nums)
+        {
+            maxVal = nums;
+        }
+    }
+
+    left = 1, right = maxVal;
+
+    while (left <= right)
+    {
+        int temp = 0;
+        mid = (right + left) / 2;
+        cout << mid << endl;
+        for (int i = 0; i < v.size(); i++)
+        {
+            temp += v[i] / mid;
+        }
+
+        if (temp < k)
+        {
+            right = mid - 1;
+        }
+        else
+        {
+            if (ans < mid)
+                ans = mid;
+            left = mid + 1;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}

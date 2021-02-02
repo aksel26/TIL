3ff64a501af9a683165214d15e5e7ca84682dd5b
김현민 <kevinxkim2023@gmail.com>
2021-02-02 18:20:41 +0900

    update onChange

--- React/react_1/src/components/UpdateContent.js
+++ React/react_1/src/components/UpdateContent.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react"
 
 class UpdateContent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: this.props.data.title,
+      desc: this.props.data.desc,
+    }
+    this.inputFormHandler = this.inputFormHandler.bind(this)
+  }
+
+  inputFormHandler(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
   render() {
     console.log("UpdateContent render")
     console.log(this.props.data)
@@ -17,10 +31,26 @@ class UpdateContent extends Component {
           }.bind(this)}
         >
           <p>
-            <input type="text" name="title" placeholder="title"></input>
+            <input
+              type="text"
+              name="title"
+              placeholder="title"
+              value={this.state.title}
+              // onChange={this.inputFormHandler.bind(this)}
+              onChange={this.inputFormHandler}
+            ></input>
           </p>
           <p>
-            <textarea name="desc" placeholder="description"></textarea>
+            {/* <textarea name="desc" placeholder="description">
+              {this.state.desc}
+            </textarea> */}
+            <textarea
+              name="desc"
+              placeholder="description"
+              value={this.state.desc}
+              // onChange={this.inputFormHandler.bind(this)}
+              onChange={this.inputFormHandler}
+            ></textarea>
           </p>
           <p>
             <input type="submit"></input>

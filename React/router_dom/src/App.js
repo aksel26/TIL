62484708d1deb58cdda66ae26ca2654c2b5a4fa2
김현민 <kevinxkim2023@gmail.com>
2021-02-12 20:07:59 +0900

    react router useParams()

--- React/router_dom/src/App.js
+++ React/router_dom/src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Route, Switch, NavLink, Link, useParams } from "react-router-dom"
+import React, { Component } from "react"
+import "./App.css"
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Home</h2>
+      Home....
     </div>
-  );
+  )
 }
 
-export default App;
+let contents = [
+  { id: 1, title: "HTML", desc: "HTML is..." },
+  { id: 2, title: "JS", desc: "JS is..." },
+  { id: 3, title: "React", desc: "React is..." },
+]
+
+function Topic() {
+  var params = useParams()
+  var topic_id = params.topic_id
+  var selected_topic = {
+    title: "No data",
+    desc: "Not found",
+  }
+  for (var i = 0; i < contents.length; i++) {
+    if (contents[i].id === Number(topic_id)) {
+      selected_topic = contents[i]
+      break
+    }
+  }
+  console.log("params:", params, "params.topic_id", params.topic_id)
+  return (
+    <div>
+      <h3>{selected_topic.title}</h3>
+      {selected_topic.desc}
+    </div>
+  )
+}
+function Topics() {
+  let lis = []
+
+  for (var i = 0; i < contents.length; i++) {
+    lis.push(
+      <li>
+        <NavLink to={"/topics/" + contents[i].id}>{contents[i].title}</NavLink>
+      </li>
+    )
+  }
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>{lis}</ul>
+      <Route path="/topics/:topic_id">
+        <Topic></Topic>
+      </Route>
+    </div>
+  )
+}
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      Contact....
+    </div>
+  )
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>React Router DOM Example</h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/topics">
+            <Topics></Topics>
+          </Route>
+          <Route path="/contact">
+            <Contact></Contact>
+          </Route>
+          <Route path="/">Not Found 404</Route>
+        </Switch>
+      </div>
+    )
+  }
+}
+
+export default App
--- React/router_dom/src/index.js
+++ React/router_dom/src/index.js
@@ -1,64 +1,9 @@
 import React from "react"
+import App from "./App"
 import ReactDOM from "react-dom"
 import "./index.css"
 import reportWebVitals from "./reportWebVitals"
-import { BrowserRouter, Route, Switch } from "react-router-dom"
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-      Home....
-    </div>
-  )
-}
-function Topics() {
-  return (
-    <div>
-      <h2>Topics</h2>
-      Topics....
-    </div>
-  )
-}
-function Contact() {
-  return (
-    <div>
-      <h2>Contact</h2>
-      Contact....
-    </div>
-  )
-}
-
-function App() {
-  return (
-    <div>
-      <h1>React Router DOM Example</h1>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/topics">Topics</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-      </ul>
-      <Switch>
-        <Route exact path="/">
-          <Home></Home>
-        </Route>
-        <Route path="/topics">
-          <Topics></Topics>
-        </Route>
-        <Route path="/contact">
-          <Contact></Contact>
-        </Route>
-        <Route path="/">Not Found 404</Route>
-      </Switch>
-    </div>
-  )
-}
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>

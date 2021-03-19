76fcc32d4a30be4dbd5aa84ac92b423893fba683
김현민 <kevinxkim2023@gmail.com>
2021-03-19 18:48:26 +0900

    문자열내맘대로정렬

--- 200615_C++/Programmers/문자열내맘대로정렬.cpp
+++ 200615_C++/Programmers/문자열내맘대로정렬.cpp
@@ -7,10 +7,10 @@ bool compare(string a, string b)
 
     if (a[N] != b[N])
     {
-        return a[N] < b[N];
+        return a[N] < b[N]; //N인덱스의 문자로 정렬
     }
     else
-        return a < b;
+        return a < b; //사전순으로 정렬
 };
 
 vector<string> solution(vector<string> strings, int n)
@@ -26,8 +26,7 @@ vector<string> solution(vector<string> strings, int n)
 
     sort(answer.begin(), answer.end(), compare);
 
-    
-    return answer;
+        return answer;
 }
 
 int main()

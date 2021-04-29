acd50114300cdf9d09b079e60bb1ce8e6fb1e274
김현민 <kevinxkim2023@gmail.com>
2021-04-29 18:46:00 +0900

    config setting

---
+++ (0729~) Node.js/nodeReact/config/prod.js
@@ -0,0 +1,4 @@
+//MONGO_URI는 Heroku 형식이름과 같아야 한다.
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+}
--- (0729~) Node.js/nodeReact/index.js
+++ (0729~) Node.js/nodeReact/index.js
@@ -2,21 +2,40 @@ const express = require("express")
 const app = express()
 const port = 5000
 const mongoose = require("mongoose")
+
+const bodyParser = require("body-parser")
+
+const { User } = require("./models/User")
+const config = require("./config/key")
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 mongoose
-  .connect(
-    "mongodb+srv://admin:qwer1234@cluster0.xxjog.mongodb.net/nodeReact?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err))
 
 app.get("/", (req, res) => {
   res.send("hello world!")
+})
+
+app.post("/register", (req, res) => {
+  //회원가입시 필요한 정보들을 client에서 가져오면
+  // 그것들을 db에 넣어준다.
+
+  // bodyparser로 인해서 json형태로 담겨있다. req.body:
+  //   {id : 'hello',
+  // password: '123'}
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({ success: true })
+  }) //save() : mongoDB의 메서드
 })
 
 app.listen(port, () => {

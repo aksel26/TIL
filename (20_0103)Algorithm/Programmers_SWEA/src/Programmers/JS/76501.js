097adae86382e36a56b3b7572e9f3fe8ce117624
aksel26 <kevinxkim2023@gmail.com>
2021-09-13 16:03:02 +0900

    음양 더하기

---
+++ (20_0103)Algorithm/Programmers_SWEA/src/Programmers/JS/76501.js
@@ -0,0 +1,14 @@
+function solution(absolutes, signs) {
+  var answer = 0
+
+  for (let i = 0; i < absolutes.length; i++) {
+    if (signs[i] === true) {
+      answer += absolutes[i]
+    } else {
+      answer -= absolutes[i]
+    }
+  }
+
+  console.log(answer)
+  return answer
+}

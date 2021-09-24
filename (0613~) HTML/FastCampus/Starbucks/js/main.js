928c98ad55a93f4c0ffb26ff6338fa5c3e2befad
aksel26 <kevinxkim2023@gmail.com>
2021-09-24 16:34:32 +0900

    뱃지

--- (0613~) HTML/FastCampus/Starbucks/js/main.js
+++ (0613~) HTML/FastCampus/Starbucks/js/main.js
@@ -13,3 +13,36 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused")
   searchInputEl.setAttribute("placeholder", "")
 })
+
+const badgeEl = document.querySelector("header .badges")
+
+// window : 브라우저의 하나의 탭(창)
+// _.throttle(함수, 시간)
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 500) {
+      //배지 숨기기
+      // badgeEl.style.display = "none"
+
+      //gsap 애니메이션 라이브러리
+      // gsap.to(요소, 지속시간, 옵션);
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+
+        //안보이는 것 뿐만 아니라 실질적으로 사라지게 해야 함.
+        display: "none",
+      })
+
+      console.log(window.scrollY)
+    } else {
+      // 배지 보이기
+      // badgeEl.style.display = "block"
+
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      })
+    }
+  }, 300)
+)

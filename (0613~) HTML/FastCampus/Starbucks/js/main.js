c9f033dcb0cefaa11856ea2efad0ecdb826455bf
aksel26 <kevinxkim2023@gmail.com>
2021-09-28 19:05:09 +0900

    swiper 기능 추가

--- (0613~) HTML/FastCampus/Starbucks/js/main.js
+++ (0613~) HTML/FastCampus/Starbucks/js/main.js
@@ -59,3 +59,28 @@ new Swiper(".notice-line .swiper-container", {
   autoplay: true,
   loop: true,
 })
+
+new Swiper(".promotion .swiper-container", {
+  slidesPerView: 3, //한번에 보여줄 슬라이드 갯수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true, //1번슬라이드가 가운데부터 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000, //0.5초에 한번씩 슬라이드가 된다.
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination", //페이지번호 요소 선택자
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+})
+
+const promotionEl = document.querySelector(".promotion")
+const promotionToggleBtn = document.querySelector(".toggle-promotion")
+let isHidePromotion = false
+promotionToggleBtn.addEventListener("click", function () {
+  isHidePromotion = !isHidePromotion
+})

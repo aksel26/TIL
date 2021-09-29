de8a596148ee1fef0aae05ef9f8acaf80ee66c7e
aksel26 <kevinxkim2023@gmail.com>
2021-09-29 18:01:48 +0900

    유튜브 & 반복애니메이션

--- (0613~) HTML/FastCampus/Starbucks/js/main.js
+++ (0613~) HTML/FastCampus/Starbucks/js/main.js
@@ -83,4 +83,33 @@ const promotionToggleBtn = document.querySelector(".toggle-promotion")
 let isHidePromotion = false
 promotionToggleBtn.addEventListener("click", function () {
   isHidePromotion = !isHidePromotion
+
+  if (!isHidePromotion) {
+    // 숨김처리
+    promotionEl.classList.add("hide")
+  } else {
+    //보임처리
+    promotionEl.classList.remove("hide")
+  }
 })
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
+    repeat: -1, //-1은 gsap라이브러리에서 무한반복을 의미함
+    yoyo: true, //한번재생된 애니메이션을 다시 뒤로 재생
+    ease: Power1.easeInOut,
+    delay: random(0, delay),
+  })
+}
+
+floatingObject(".floating1", 1, 15)
+floatingObject(".floating2", 0.5, 15)
+floatingObject(".floating3", 1.5, 20)

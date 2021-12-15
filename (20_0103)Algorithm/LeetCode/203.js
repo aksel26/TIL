ba4ba679c73ce7b39ba5660863ced2fe5b76d149
aksel26 <kevinxkim2023@gmail.com>
2021-12-15 16:11:30 +0900

    203. Remove Linked List Elements

---
+++ (20_0103)Algorithm/LeetCode/203.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+  if (head === null) return head;
+  let dummy = new ListNode(0);
+  dummy.next = head;
+  let cur = dummy;
+
+  while (cur.next !== null) {
+    if (cur.next.val === val) {
+      cur.next = cur.next.next;
+    } else {
+      cur = cur.next;
+    }
+  }
+
+  return dummy.next;
+};
